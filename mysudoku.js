93fc9b17dfec4990d20a551e1bbf10abf49e96c5
IyLias <iliaskyrin9707@gmail.com>
2022-06-05 16:46:33 +0900

    finished defining functions

--- mysudoku.js
+++ mysudoku.js
@@ -39,6 +39,7 @@ class SudokuGame{
             this.solved_sudokuMap[i] = new Array(BOARD_SIZE);
     }
 
+
     initGameDatabase(){
 
         // 추후 데이터 넣을 예정..
@@ -56,6 +57,7 @@ class SudokuGame{
     }
 
 
+
     // set values of sudoku game map for target idx
     setGameDatabase(target,map){
 
@@ -66,7 +68,7 @@ class SudokuGame{
 
     // for pressed new problem button, reset for new game data from game database
     setRandomSudokuMap(){
-        let randomNumber = Math.floor((Math.random()*10));
+        let randomNumber = Math.floor((Math.random()*GAME_DATABASE));
         this.setSudokuMap(this.sudokuMapDatas[randomNumber]);
     }
 
@@ -150,24 +152,125 @@ class SudokuGame{
 
     checkUserAnswer(){
 
+        let currentCheckNumber = 1;
+        const bitNumber = [0,1,2,4,8,16,32,64,128,256];
+
+        // check every row for 1~9 written only one time
+        for(var i=0;i<BOARD_SIZE;i++){
+            currentCheckNumber = 1;
+            for(var j=0;j<BOARD_SIZE;j++)
+                currentCheckNumber |= bitNumber[this.user_sudokuMap[i][j]];
+            
+            if(currentCheckNumber !== CHECK_NUMBER)
+                return false;
+        }
+
+        // check every column for 1~9 written only one time
+        for(var i=0;i<BOARD_SIZE;i++){
+            currentCheckNumber = 1;
+            for(var j=0;j<BOARD_SIZE;j++)
+                currentCheckNumber |= bitNumber[this.user_sudokuMap[i][j]];
+            
+            if(currentCheckNumber !== CHECK_NUMBER)
+                return false;
+        }
 
 
+        // check every 3x3 square for 1~9 written only one time
+        const squareIdx = [0,3,6];
+        for(var i=0;i<3;i++)
+            for(var j=0;j<3;j++){
+                currentCheckNumber = 1;
+                for(var x=squareIdx[i];x<squareIdx[i]+3;x++)
+                    for(var y=squareIdx[j];y<squareIdx[j]+3;y++)
+                        currentCheckNumber |= bitNumber[this.user_sudokuMap[x][y]]; 
 
+
+                if(currentCheckNumber !== CHECK_NUMBER)
+                    return false;
+            }
+        
+        // if passed 3 cases tests, then it's correct answer
+        return true;
     }
 
 
     solve(){
+        
+        let emptySpot = this.find_emptySpot();
+        while(emptySpot !== -1){
+            let r = emptySpot[0], c = emptySpot[1];
+            console.log(emptySpot);
+
+            for(var p=1;p<10;p++){
+                // if current number p is not proper to current empty spot,
+                // then escape flag turns on true
+                let escape_flag = false;
+
+                // check row of current empty Spot //
+                for(var v=0;v<BOARD_SIZE;v++)
+                    if(this.solved_sudokuMap[r][v] === p){
+                        escape_flag = true;
+                        break;
+                    }
+                
+                if(escape_flag === true)
+                    continue;
+                // check row of current empty spot //
+
+
+                // check column of current empty Spot //
+                for(var v=0;v<BOARD_SIZE;v++)
+                if(this.solved_sudokuMap[v][c] === p){
+                    escape_flag = true;
+                    break;
+                }
+
+                if(escape_flag === true)
+                    continue;
+                // check column of current empty spot //
 
 
 
+                // check 3x3 square of empty spot //
+                for(var i=(Math.floor(r/3))*3;i<(Math.floor(r/3))*3+3;i++){
+                    for(var j=(Math.floor(c/3))*3;j<(Math.floor(c/3))*3+3;j++)
+                        if(this.solved_sudokuMap[i][j] === p){
+                            escape_flag = true;
+                            break;
+                        }
+                    
+                    if(escape_flag === true)
+                        break;
+                }
 
+                if(escape_flag === true)
+                    continue;
+                // check 3x3 square of empty spot //
+                
+                
+                // set value p on spot [r,c]
+                this.solved_sudokuMap[r][c] = p;
+                if(this.solve() === -1)
+                    this.solved_sudokuMap[r][c] = 0;
+                else    
+                    return 0;
+            }
+
+            if(this.solved_sudokuMap[r][c] === 0)
+                return -1;
+        }
+        
+        return 0;
     }
 
-
+    // check user sudoku map and answer same
     checkAnswer(){
 
-
-
+        if(this.checkUserAnswer() === true)
+            return true;
+        else 
+            return false;
     }
 
 };
@@ -207,9 +310,9 @@ $solveButton.addEventListener("click",function(){
 const $checkButton = document.getElementById('check_button');
 $checkButton.addEventListener("click",function(){
     if(mySudoku.checkAnswer() == true)
-        alert("정답!! 좀 SMART한 친군가?");
+        alert("correct!! you solved ))");
     else
-        alert("틀렸어.. 멍청이");
+        alert("wrong... Try again");
 });
 
 
@@ -237,19 +340,167 @@ const EMPTY_COLOR = '#FFFFFF';
 
 // change color of specific position
 function changeColorOfSpot(r,c,color){
+    const number= ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const $spot = document.querySelector('#' + number[r] + '_' + number[c]);
+    console.log($spot);
 
-
+    $spot.style.backgroundColor = color;
 }
 
 // print number for user input
-function outputNumber(r,c,color){
-
+function outputNumber(r,c,num){
+    const number= ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const $spot = document.querySelector('#' + number[r] + '_' + number[c]);
     
+    $spot.innerHTML = num !== 0 ? num : "";
+    $spot.style.fontWeight = 'normal';
+    $spot.style.color = 'black';
 }
 
 
+changeColorOfSpot(user_r,user_c,USER_COVER_COLOR);
+
+function keyDownEvent(e){
+    switch(e.keyCode){
+
+        // left
+        case 37:
+            changeColorOfSpot(user_r,user_c,EMPTY_COLOR);
+            user_c-=1;
+            if(user_c < 1)
+                user_c = 1;
+
+            changeColorOfSpot(user_r,user_c,USER_COVER_COLOR);
+            break;
+
+        // up
+        case 38:
+            changeColorOfSpot(user_r,user_c,EMPTY_COLOR);
+            user_r-=1;
+            if(user_r < 1)
+                user_r = 1;
+
+            changeColorOfSpot(user_r,user_c,USER_COVER_COLOR);
+            break;
+
+        // right
+        case 39:
+            changeColorOfSpot(user_r,user_c,EMPTY_COLOR);
+            user_c+=1;
+            if(user_c > BOARD_SIZE)
+                user_c = BOARD_SIZE;
+
+            changeColorOfSpot(user_r,user_c,USER_COVER_COLOR);
+            break;
+
+        // down
+        case 40:
+            changeColorOfSpot(user_r,user_c,EMPTY_COLOR);
+            user_r+=1;
+            if(user_r > BOARD_SIZE)
+                user_r = BOARD_SIZE;
+
+            changeColorOfSpot(user_r,user_c,USER_COVER_COLOR);
+            break;
 
 
+
+        //case for pressing number -> the number pressed by user should be output on page
+
+        // number 1
+        case 49:
+            if(mySudoku.base_sudokuMap[user_r-1][user_c-1] === 0){
+                outputNumber(user_r,user_c,1);
+                mySudoku.user_sudokuMap[user_r-1][user_c-1] = 1;
+            }
+            break;
+
+        // number 2
+        case 50:
+            if(mySudoku.base_sudokuMap[user_r-1][user_c-1] === 0){
+                outputNumber(user_r,user_c,2);
+                mySudoku.user_sudokuMap[user_r-1][user_c-1] = 2;
+            }
+            break;
+
+        // number 3
+        case 51:
+            if(mySudoku.base_sudokuMap[user_r-1][user_c-1] === 0){
+                outputNumber(user_r,user_c,3);
+                mySudoku.user_sudokuMap[user_r-1][user_c-1] = 3;
+            }
+            break;
+
+
+        // number 4
+        case 52:
+            if(mySudoku.base_sudokuMap[user_r-1][user_c-1] === 0){
+                outputNumber(user_r,user_c,4);
+                mySudoku.user_sudokuMap[user_r-1][user_c-1] = 4;
+            }
+            break;
+    
+
+        // number 5
+        case 53:
+            if(mySudoku.base_sudokuMap[user_r-1][user_c-1] === 0){
+                outputNumber(user_r,user_c,5);
+                mySudoku.user_sudokuMap[user_r-1][user_c-1] = 5;
+            }
+            break;
+
+
+        // number 6
+        case 54:
+            if(mySudoku.base_sudokuMap[user_r-1][user_c-1] === 0){
+                outputNumber(user_r,user_c,6);
+                mySudoku.user_sudokuMap[user_r-1][user_c-1] = 6;
+            }
+            break;
+
+        
+        // number 7
+        case 55:
+            if(mySudoku.base_sudokuMap[user_r-1][user_c-1] === 0){
+                outputNumber(user_r,user_c,7);
+                mySudoku.user_sudokuMap[user_r-1][user_c-1] = 7;
+            }
+            break;
+
+        
+        // number 8
+        case 56:
+            if(mySudoku.base_sudokuMap[user_r-1][user_c-1] === 0){
+                outputNumber(user_r,user_c,8);
+                mySudoku.user_sudokuMap[user_r-1][user_c-1] = 8;
+            }
+            break;
+
+
+        // number 9
+        case 57:
+            if(mySudoku.base_sudokuMap[user_r-1][user_c-1] === 0){
+                outputNumber(user_r,user_c,9);
+                mySudoku.user_sudokuMap[user_r-1][user_c-1] = 9;
+            }
+            break;
+
+
+        // number 0
+        case 48:
+            if(mySudoku.base_sudokuMap[user_r-1][user_c-1] === 0){
+                outputNumber(user_r,user_c,0);
+                mySudoku.user_sudokuMap[user_r-1][user_c-1] = 0;
+            }
+            break;
+
+
+    }
+
+
+
+
+}
 
 
 
